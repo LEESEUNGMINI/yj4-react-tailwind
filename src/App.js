d11c85695331c86be0d31ecca6e343618691d88f
LEESEUNGMINI <dltmdals7295@naver.com>
2023-12-29 09:41:06 +0900

    21

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div className=" bg-slate-600 w-full h-[300px]">zzzz</div>;
+  return (
+    <div className=" bg-slate-600 w-full h-[300px]">
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
